8301f7c23a95bba57aea8159ec133743eb19abc0
Marwan <emef4z@gmail.com>
2019-07-09 21:07:56 +0100

    Add adapter and data to populate recycler view

--- app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -68,6 +68,10 @@ public class NoteListActivity extends AppCompatActivity {
         final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManager);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        final NoteRecyclerAdapter noteRecyclerAdapter = new NoteRecyclerAdapter(this,notes);
+        recyclerNotes.setAdapter(noteRecyclerAdapter);
     }
 
 }
---
+++ app/src/main/java/com/example/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,62 @@
+package com.example.notekeeper;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+    private final List<NoteInfo> mNotes;
+
+    public NoteRecyclerAdapter(Context mContext, List<NoteInfo> mNotes) {
+        this.mContext = mContext;
+        this.mNotes = mNotes;
+        //Creating a ayout using layout inflater
+        mLayoutInflater = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override  //What recycler view uses to create pool of views.
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list,viewGroup,false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        NoteInfo note = mNotes.get(position);
+        viewHolder.mTextCourse.setText(note.getCourse().getTitle());
+        viewHolder.mTextTitle.setText(note.getTitle());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        //To know how much data there is
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+
+        }
+    }
+}
