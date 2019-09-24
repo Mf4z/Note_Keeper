fd82760c4fbe0571e1ccf89a677df1ca1148a9aa
Marwan <emef4z@gmail.com>
2019-09-24 15:02:06 +0400

    Add constraints to columns

--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -11,8 +11,8 @@ public final class NoteKeeperDatabaseContract {
         //CREATE TABLE course_info (course_id,course_title)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+ TABLE_NAME+ " ("+
-                        COLUMN_COURSE_ID +", "+
-                        COLUMN_COURSE_TITLE + ")";
+                        COLUMN_COURSE_ID +"TEXT UNIQUE NOT NULL, "+
+                        COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
     public static final class NoteInfoEntry{
@@ -23,8 +23,8 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+ TABLE_NAME+ " ("+
-                        COLUMN_NOTE_TITLE +", "+
-                        COLUMN_NOTE_TEXT + ", "+
-                        COLUMN_COURSE_ID +")";
+                        COLUMN_NOTE_TITLE +",TEXT NOT NULL "+
+                        COLUMN_NOTE_TEXT + "TEXT, "+
+                        COLUMN_COURSE_ID +" TEXT NOT NULL)";
     }
 }
