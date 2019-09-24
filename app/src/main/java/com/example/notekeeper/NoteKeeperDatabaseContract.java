03821710f15ae872f20f228fb991a3f071995085
Marwan <emef4z@gmail.com>
2019-09-24 14:47:21 +0400

    Implement DB contract class

---
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -0,0 +1,30 @@
+package com.example.notekeeper;
+
+public final class NoteKeeperDatabaseContract {
+
+    private NoteKeeperDatabaseContract() {} //make non-creatable
+    public static final class CourseInfoEntry {
+        public static final String TABLE_NAME = "course_info";
+        public static final String COLUMN_COURSE_ID = "course_id";
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+
+        //CREATE TABLE course_info (course_id,course_title)
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE "+ TABLE_NAME+ " ("+
+                        COLUMN_COURSE_ID +", "+
+                        COLUMN_COURSE_TITLE + ")";
+    }
+
+    public static final class NoteInfoEntry{
+        public static final String TABLE_NAME = "note_info";
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+        public static final String COLUMN_COURSE_ID = "course_id";
+
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE "+ TABLE_NAME+ " ("+
+                        COLUMN_NOTE_TITLE +", "+
+                        COLUMN_NOTE_TEXT + ", "+
+                        COLUMN_COURSE_ID +")";
+    }
+}
