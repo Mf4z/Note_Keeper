199df78500c51607a8fed53256a23b386610cfba
Marwan <emef4z@gmail.com>
2019-10-22 14:32:30 +0400

    Returning the appropriate mime type

--- app/src/main/java/com/example/notekeeper/NoteKeeperProvider.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProvider.java
@@ -1,6 +1,7 @@
 package com.example.notekeeper;
 
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
@@ -16,6 +17,7 @@ import static com.example.notekeeper.NoteKeeperProviderContract.*;
 
 public class NoteKeeperProvider extends ContentProvider {
 
+    private static final String MIME_VENDOR_TYPE = "vnd." + AUTHORITY + ".";
     private NoteKeeperOpenHelper mDbOpenHelper;
 
     private static UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -84,9 +86,33 @@ public class NoteKeeperProvider extends ContentProvider {
 
     @Override
     public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String mimeType = null;
+
+        int uriMatch = sUriMatcher.match(uri);
+        switch (uriMatch){
+            case COURSES : //"item" for single and "dir" for mutiple rows
+                //vnd.android.cursor.dir/vnd.com.notekeeper.provider.courses
+                mimeType = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + MIME_VENDOR_TYPE + Courses.PATH;
+
+                break;
+
+            case NOTES :
+                mimeType = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + MIME_VENDOR_TYPE + Notes.PATH;
+
+                break;
+
+            case NOTES_EXPANDED :
+                mimeType = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + MIME_VENDOR_TYPE + Notes.PATH_EXPANDED;
+
+                break;
+
+            case NOTES_ROW :
+                mimeType = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + MIME_VENDOR_TYPE + Notes.PATH;
+
+                break;
+        }
+
+        return mimeType;
     }
 
     @Override
